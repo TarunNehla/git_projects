de21e14e15cce062252f77020c281f9d6f1272b4
Tarun Dhillon <tarundhillon14330@gmail.com>
2023-07-04 18:59:24 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define endl "\n"
+
+void solve(){
+    int a = 5;
+    int b = 6;
+    cout<<a+b<<endl;  
+    
+return;
+}
+
+signed main()
+{
+ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+int t  = 1;
+while(t--){
+solve();
+}
+return 0;
+}
